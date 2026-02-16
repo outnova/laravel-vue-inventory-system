640a3d73c8870e42abb9e714e1113a4457e5baba
outnova <bjalastre@gmail.com>
2026-02-16 13:26:47 -0400

    fix: axiosClient baseurl, replace https to http

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://localhost:8000/api',
+    baseURL: 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
