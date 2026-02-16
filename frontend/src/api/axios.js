3b61817d0edcab48e601572af75f4cfb623970ac
outnova <bjalastre@gmail.com>
2026-02-16 12:28:07 -0400

    feat: create axios client

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+    baseURL: 'https://localhost:8000/api',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+
+axiosClient.interceptors.response.use(
+    response => response,
+    error => {
+        console.error('API Error:', error.response?.data || error.message)
+        return Promise.reject(error)
+    }
+)
+
+export default axiosClient
