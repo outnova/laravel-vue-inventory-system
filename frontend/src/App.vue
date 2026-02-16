1d3adec30d82187908e7f95c062cd191a0ba66f2
outnova <bjalastre@gmail.com>
2026-02-16 13:22:36 -0400

    feat: setup App.vue, import ProductList

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductList from './components/ProductList.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-100">
+    <nav class="bg-white shadow-sm p-4 mb-4">
+      <div class="container mx-auto font-bold text-xl text-blue-600 italic">
+        INVENTORY PRO
+      </div>
+    </nav>
+    <main class="container mx-auto">
+      <ProductList />
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
