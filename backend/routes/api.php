c917a762cb9d12f26301cb5718cbc5f275bafa92
outnova <bjalastre@gmail.com>
2026-02-16 11:55:40 -0400

    feat: add products api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('products', ProductController::class);
