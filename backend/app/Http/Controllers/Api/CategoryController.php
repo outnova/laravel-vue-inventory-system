e44eb9eaa9e526b0e87c0e96b1a2be6fcd96e427
outnova <bjalastre@gmail.com>
2026-02-16 13:23:10 -0400

    feat: create category controller

---
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //Return all categories order by name
+        return CategoryResource::collection(Category::orderBy('name')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|unique:categories,name|max:255',
+            'description' => 'nullable|string'
+        ]);
+
+        $category = Category::create($data);
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
