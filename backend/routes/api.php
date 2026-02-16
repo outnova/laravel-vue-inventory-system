517b3ae97345b931947ec56f6e89931f9b2861d0
outnova <bjalastre@gmail.com>
2026-02-16 13:23:48 -0400

    feat: add categories api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
